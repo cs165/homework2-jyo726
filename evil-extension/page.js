023731526174a317dfa5b0ba703f59373979db1c
jyo726 <u03410109@ccu.edu.tw>
2019-04-29 20:22:09 +0800

    Add files via upload

--- evil-extension/page.js
+++ evil-extension/page.js
@@ -10,11 +10,24 @@ const MATCH_LIST = {
   'THEY\'RE': 'THERE'
 };
 
-function transformTextNodes(node) {
+function transformtxtNodes(node) {
   // TODO(you): Implement this function! See HW spec for details.
+  if (node.nodeType == Node.txt_NODE){
+    const splittxt = node.txtContent.split(" ");
+    for (let i = 0; i < splittxt.length; i ++){
+      if (MATCH_LIST.hasOwnProperty(splittxt[i].trim())){
+        splittxt[i] = splittxt[i].replace(splittxt[i].trim(), MATCH_LIST[splittxt[i].trim()]);
+      }
+    }
+    node.txtContent = splittxt.join(" ");
+  }
+  for (const child of node.childNodes){
+    transformtxtNodes(child);
+  }
 }
 
-transformTextNodes(document.body);
+transformtxtNodes(document.body);
 
 // Log statement to test that the extension loaded properly.
 console.log('Evil extension loaded!');
+console.log('Extension updated');
